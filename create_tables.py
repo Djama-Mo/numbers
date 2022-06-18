13596e0d661664b49d8420ab0c325ff5b120e706
Djama-Mo <djamamosss@gmail.com>
2022-06-18 07:41:10 +0300

    DataBase and 1 Table

---
+++ create_tables.py
@@ -0,0 +1,40 @@
+import psycopg2
+from config import config
+
+
+def connect():
+    """ Connect to the PostgreSQL database server """
+    comm = """
+        CREATE TABLE testing (
+            "№" SMALLINT NOT NULL,
+            "заказ, №" VARCHAR(127) NOT NULL,
+            "стоимость, $" INTEGER NOT NULL,
+            "стоимость в руб." DECIMAL(10, 2) NOT NULL,
+            "срок поставки" DATE
+        )
+        """
+    conn = None
+    try:
+        # read connection parameters
+        params = config()
+
+        # connect to the PostgreSQL server
+        print('Connecting to the PostgreSQL database...')
+        conn = psycopg2.connect(**params)
+        cur = conn.cursor()
+        # create table one by one
+        cur.execute(comm)
+        # close communication with the PostgreSQL database server
+        cur.close()
+        # commit the changes
+        conn.commit()
+    except (Exception, psycopg2.DatabaseError) as error:
+        print(error)
+    finally:
+        if conn is not None:
+            conn.close()
+            print('Database connection closed.')
+
+
+if __name__ == '__main__':
+    connect()
